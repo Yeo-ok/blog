a1fd546cb3ed2bb4dedd8692810e52d0b48ebc9e
yeo-ok <wls990728@naver.com>
2024-01-12 16:42:03 +0900

    2024-01-12 마무리

--- src/test/java/com/it/blog/controller/BlogApiControllerTest.java
+++ src/test/java/com/it/blog/controller/BlogApiControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
